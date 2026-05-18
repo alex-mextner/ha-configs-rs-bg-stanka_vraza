d3c66abaf79866333d8182fbf3d4498a7324e038
Home Assistant <homeassistant@local>
2026-05-18 15:40:36 +0200

    rm js panels

--- www/panels/code.js
+++
@@ -1,6 +0,0 @@
-class HaPanelCode extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = '<iframe src="http://homeassistant-code-server-1:8080" style="width:100%;height:100%;border:none;"></iframe>';
-  }
-}
-customElements.define("ha-panel-code", HaPanelCode);
